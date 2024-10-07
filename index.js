64ad5ce124fccaae4669286b4f5165a3e91f017b
harshit-kumar78 <harshit3551@gmail.com>
2024-10-07 21:45:30 +0530

    create operations

--- index.js
+++ index.js
@@ -7,7 +7,31 @@ async function main() {
     //connect to the mongoDB custer
     await client.connect();
     //make db interaction
-    await listDatabases(client);
+    // await listDatabases(client);
+    // create a single user
+    await createUser(client, {
+      name: "Alice3",
+      email: "alice@example3.com",
+      password: "password123",
+    });
+    // create multiple user
+    await createMultipleUser(client, [
+      {
+        name: "Alice",
+        email: "alice@example.com",
+        password: "password123",
+      },
+      {
+        name: "Bob",
+        email: "bob@example.com",
+        password: "securePassword456",
+      },
+      {
+        name: "Charlie",
+        email: "charlie@example.com",
+        password: "charliePass789",
+      },
+    ]);
   } catch (err) {
     console.log(err);
   } finally {
@@ -30,4 +54,28 @@ async function listDatabases(client) {
   databaseList.databases.forEach(function (db) {
     console.log(db.name);
   });
+}
+
+//create new user
+async function createUser(client, newUser) {
+  if (newUser) {
+    const result = await client
+      .db("sample_mflix")
+      .collection("users")
+      .insertOne(newUser);
+    console.log(`New user created with the following id: ${result.insertedId}`);
+  }
+}
+
+//create multiple user
+async function createMultipleUser(client, multipleUser) {
+  const result = await client
+    .db("sample_mflix")
+    .collection("users")
+    .insertMany(multipleUser);
+
+  console.log(
+    `${result.insertedCount} new listing(s) created with the following id(s):`
+  );
+  console.log(result.insertedIds);
 }
