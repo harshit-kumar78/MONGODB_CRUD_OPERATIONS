545210e34a94f89257bde5980508612306ef1196
harshit-kumar78 <harshit3551@gmail.com>
2024-10-09 11:15:49 +0530

    read operations

--- index.js
+++ index.js
@@ -2,34 +2,17 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config(); //import the env files
 
 async function main() {
+  //create the mongo client instance
   const client = new MongoClient(process.env.MONGO_CLUSTER_URI);
   try {
     //connect to the mongoDB custer
     await client.connect();
-    // create a single user
-    await createUser(client, {
-      name: "Alice3",
-      email: "alice@example3.com",
-      password: "password123",
+    // read one user
+    // await findOneUserByName(client, (userName = ""));
+    await findMinimumRuntimeYearAndMostRecentRelease(client, {
+      minimumRuntime: 10,
+      minimumYear: 2000,
     });
-    // create multiple user
-    await createMultipleUser(client, [
-      {
-        name: "Alice",
-        email: "alice@example.com",
-        password: "password123",
-      },
-      {
-        name: "Bob",
-        email: "bob@example.com",
-        password: "securePassword456",
-      },
-      {
-        name: "Charlie",
-        email: "charlie@example.com",
-        password: "charliePass789",
-      },
-    ]);
   } catch (err) {
     console.log(err);
   } finally {
@@ -76,4 +59,62 @@ async function createMultipleUser(client, multipleUser) {
     `${result.insertedCount} new listing(s) created with the following id(s):`
   );
   console.log(result.insertedIds);
+}
+
+//Print an user details with the given name
+
+async function findOneUserByName(client, userName) {
+  const result = await client
+    .db("sample_mflix")
+    .collection("users")
+    .findOne({ name: userName });
+  if (result) {
+    console.log(
+      `Found a listing in the collection with the name '${userName}':`
+    );
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${userName}'`);
+  }
+}
+
+async function findMinimumRuntimeYearAndMostRecentRelease(
+  client,
+  {
+    minimumRuntime = 0,
+    minimumYear = 0,
+    MaximumNumberOfResults = Number.MAX_SAFE_INTEGER,
+  }
+) {
+  const cursor = await client
+    .db("sample_mflix")
+    .collection("movies")
+    .find({
+      runtime: { $gte: minimumRuntime },
+      year: { $gte: minimumYear },
+    })
+    .sort({ released: -1 })
+    .limit(5);
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    console.log(
+      `Found listing(s) with at least ${minimumRuntime} runtime and ${minimumYear} year:`
+    );
+    results.forEach((result, i) => {
+      const date = new Date(result.released).toDateString();
+
+      console.log();
+      console.log(`${i + 1}. name: ${result.plot}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   runtime: ${result.runtime}`);
+      console.log(`   year: ${result.year}`);
+      console.log(`   most recent release date: ${date}`);
+    });
+  } else {
+    console.log(
+      `No movies found with at least ${minimumRuntime} bedrooms and ${minimumYear} bathrooms`
+    );
+  }
 }
